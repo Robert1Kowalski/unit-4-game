4298d188176a95179e00f1d7d0b8d0b2a1732818
Robert1Kowalski <kowals.robert@gmail.com>
2019-06-06 17:55:38 -0600

    added html links and basic layout, created four spots for the crystals

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,18 @@
-// create variables to track 
-
- 
-
-
-
-
-
-
-
-
-
-
-
-// The player will have to guess the answer, just like in Word Guess. This time, though, the player will guess with numbers instead of letters. 
-// Here's how the app works:
-
-     //a game with 4 crystals and randomResult 
+    //a game with 4 crystals and randomResult
     //every crystal needs to have a random number between 1-12
     //A new number should be generated everytime we win or lost, to those 4 crystals 
     //When clicking any CRYSTAL, it should be adding with the previous result until it equals to the total score
     //If it is greater than the random result, then we lost and decrememt a lost counter
     //If it is equal, then we increment a win counter
+
+    // 1a) create random result, lost, win
+    var random_result;
+    var lost;
+    var win; 
+
+    // 1b ) build 4 crystals
+    for (var i = 0; i < 4; i++){
+        var crystal = $("<div>");
+        $(".crystal").append(crystal);  //this should put 4 divs into "crystal" div on html
+        console.log("hello)")
+    }
