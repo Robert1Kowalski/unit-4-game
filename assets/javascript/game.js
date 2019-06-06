b05cec1cd7adf81694053c7d8a5962ee9f422025
Robert1Kowalski <kowals.robert@gmail.com>
2019-06-06 17:31:09 -0600

    PsudoCoded javascript file

---
+++ assets/javascript/game.js
@@ -0,0 +1,23 @@
+// create variables to track 
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+// The player will have to guess the answer, just like in Word Guess. This time, though, the player will guess with numbers instead of letters. 
+// Here's how the app works:
+
+     //a game with 4 crystals and randomResult 
+    //every crystal needs to have a random number between 1-12
+    //A new number should be generated everytime we win or lost, to those 4 crystals 
+    //When clicking any CRYSTAL, it should be adding with the previous result until it equals to the total score
+    //If it is greater than the random result, then we lost and decrememt a lost counter
+    //If it is equal, then we increment a win counter
