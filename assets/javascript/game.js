b75fa182abc66ea8f3f1364d596f251eb8f4c6a4
Robert1Kowalski <kowals.robert@gmail.com>
2019-06-06 19:11:15 -0600

    created target random_result random function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,44 @@
-//a game with 4 crystals and randomResult
-//every crystal needs to have a random number between 1-12
-//A new number should be generated everytime we win or lost, to those 4 crystals 
+//1A) a game with 4 crystals and 1b) randomResult
+// 2) every crystal needs to have a random number between 1-12
+// 3)A new number should be generated everytime we win or lost, to those 4 crystals 
 //When clicking any CRYSTAL, it should be adding with the previous result until it equals to the total score
 //If it is greater than the random result, then we lost and decrememt a lost counter
 //If it is equal, then we increment a win counter
 
-// 1a) create random result, lost, win
+
+
+
+
+
+
+
+
+
+// create random result, lost, win variables
 var random_result;
 var lost;
 var win;
 
-// 1b ) build 4 crystals
+// 1A ) build 4 crystals
+//1B)  build randomResult
+random_result = Math.floor(Math.random() * 120) + 19; //builds target randomResult 
+console.log(random_result)
+$("#result").html('Random Result: ' + random_result )
 // 2) create randomNumber
 for (var i = 0; i < 4; i++) {
 
-    var  randomNumber = Math.floor(Math.random () * 12); // built out randomNumber generator
-    console.log(randomNumber)
+    var  randomNumber  = Math.floor(Math.random () * 12) + 19; // built out randomNumber generator, will asign a randomNumber to each div
+    
     var crystal = $("<div>");
-    crystal.attr("class", 'crystal'); //
+    crystal.attr({
+        "class": 'crystal',
+        "data-random": randomNumber, //assigned dataRandom to each of the divs
+    
+    
+    }); 
 
     $(".crystals").append(crystal);  //this should put 4 divs into "crystal" div on html
 }
+//3) 
 
 
